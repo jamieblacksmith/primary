6827aee03c958dfca5214a5f8c4aee423d2b6fa5
Jamie <83793358+jamieblacksmith@users.noreply.github.com>
2021-05-07 15:09:47 +1200

    Create token.sol

---
+++ contracts/token.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.5;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract SimpleToken is Context, ERC20, ERC20Detailed {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) public ERC20Detailed(name, symbol, 18) {
+        _mint(_msgSender(), initialSupply);
+    }
+}
