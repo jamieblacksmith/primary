c18bd0d78c1a3e94e55cff880e89277f34c2ae5c
Jamie <83793358+jamieblacksmith@users.noreply.github.com>
2021-05-07 15:10:25 +1200

    Create ILO.sol

---
+++ contracts/ILO.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.5;
+
+import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
+
+contract SimpleCrowdsale is Crowdsale {
+    constructor(
+        uint256 rate,
+        address payable wallet,
+        IERC20 token
+    ) public Crowdsale(rate, wallet, token) {}
+}
