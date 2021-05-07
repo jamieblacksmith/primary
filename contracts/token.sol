6083a9c6a18bc636e0b7ae59e038e75e7b923a83
Jamie <83793358+jamieblacksmith@users.noreply.github.com>
2021-05-07 15:17:52 +1200

    Update token.sol

--- contracts/token.sol
+++ contracts/token.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.5;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract SimpleToken is Context, ERC20, ERC20Detailed {
+contract Token is Context, ERC20, ERC20Detailed {
     constructor(
         string memory name,
         string memory symbol,
