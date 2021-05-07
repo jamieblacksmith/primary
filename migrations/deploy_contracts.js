03c61467cf8cb03420bc0ed9bad7bb32f4840236
Jamie <83793358+jamieblacksmith@users.noreply.github.com>
2021-05-07 15:11:33 +1200

    Update deploy_contracts.js

--- migrations/deploy_contracts.js
+++ migrations/deploy_contracts.js
@@ -1,29 +1,12 @@
-const Token = artifacts.require("Token");
-const ICO = artifacts.require("ICO");
+const SimpleToken = artifacts.require("SimpleToken");
+const SimpleCrowdsale = artifacts.require("SimpleCrowdsale");
 
-module.exports = async function (deployer) {
-  const totalSupply = 10000000; //10M
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(SimpleToken, 'Simple Token', 'SIM', '10000000000000000000000');
+  const token = await SimpleToken.deployed();
+  
+  await deployer.deploy(SimpleCrowdsale, 1, accounts[0], token.address);
+  const crowdsale = await SimpleCrowdsale.deployed();
 
-  //Token
-  await deployer.deploy(
-    Token,
-    'My Personal Token', //name
-    '$MY_NAME',          //sticker
-    totalSupply
-  );
-  const token = await Token.deployed();
-
-  //ICO
-  await deployer.deploy(
-    ICO,
-    token.address,
-    592200,                         // duration (592200s = 1 week)
-    web3.utils.toWei('2', 'milli'), // price of 1 token in DAI (wei) (= 0.002 DAI. 0.002 * 10M = 20,000 DAI ~= 20,000 USD)
-    totalSupply,                    //_availableTokens for the ICO. can be less than maxTotalSupply
-    200,                            //_minPurchase (in DAI)
-    5000                            //_maxPurchase (in DAI)
-  );
-  const ico = await ICO.deployed();
-  await token.updateAdmin(ico.address);
-  await ico.start();
+  token.transfer(crowdsale.address, await token.totalSupply())
 };
